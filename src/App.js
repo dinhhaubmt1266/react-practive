049300757fca9cc49d48bdcf145009d067a636bf
TranDinhHau1402 <hau.tranbmt1266@hcmut.edu.vn>
2023-09-07 10:48:17 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import './App.css';
+import './App.scss';
 import Button from './components/button/Button';
 import Card from './components/card/Card';
 import CardList from './components/card/CardList';
+import Header from './components/react_pratice/Header';
+import TableUsers from './components/react_pratice/TableUsers';
 import Toggle from './components/state/Toggle';
 import Game from './components/tictactoe/Game';
 import YoutubeList from './components/youtube/YoutubeList';
+import Container from 'react-bootstrap/Container';
 
 
 function App() {
@@ -17,8 +20,9 @@ function App() {
     //   Cần thêm đau bao nhiêu để em thôi hững hờ 
     //   </h4>
     // </YoutubeList>
-    <div>
-      
+    <div className='app-container'>
+      <Header></Header>
+      <TableUsers></TableUsers>
     </div>
   );
 }
---
+++ src/components/react_pratice/TableUsers.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TableUsers = (props) => {
+    return (
+        <div>
+            TableUsers
+        </div>
+    );
+};
+
+export default TableUsers;
